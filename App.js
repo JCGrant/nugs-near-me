9f5389603a6bc6ee183efdba00bcfa244b9e587b
JCGrant <jamescolin.grant@gmail.com>
2017-07-08 15:03:26 +0100

    Change Marker icon to a nugget

--- App.js
+++ App.js
@@ -52,6 +52,7 @@ export default class App extends React.Component {
           <MapView.Marker
             key={i}
             coordinate={marker.coordinate}
+            image={require('./assets/nugget.png')}
           />
         ))}
       </MapView>
