5c66986ec9aad3b2b365da5062574174e8e394b9
JCGrant <jamescolin.grant@gmail.com>
2017-07-09 23:42:52 +0100

    Return list of users as JSON to hello world server

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -1,18 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Web.Scotty
 import Data.Text.Internal.Lazy (Text)
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Monoid ((<>))
+import GHC.Generics (Generic)
+
+
+data User = User { userId :: Int, userName :: String }
+  deriving (Show, Generic)
+
+instance ToJSON User
+instance FromJSON User
+
+bob :: User
+bob = User { userId = 1, userName = "bob" }
+
+jenny :: User
+jenny = User { userId = 2, userName = "jenny" }
+
+allUsers :: [User]
+allUsers = [bob, jenny]
+
 
 hello :: ActionM ()
 hello = do
   name <- param "name"
   text ("hello " <> name <> "!")
 
+users :: ActionM ()
+users = do
+  json allUsers
+
 routes :: ScottyM ()
 routes = do
   get "/hello/:name" hello
+  get "/users" users
 
 main :: IO ()
 main = do
