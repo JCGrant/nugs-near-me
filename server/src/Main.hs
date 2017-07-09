2901d4cd7964055ced3a456574526d32f020e3d3
JCGrant <jamescolin.grant@gmail.com>
2017-07-09 23:18:41 +0100

    Setup Scotty hello world server

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -1,5 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Web.Scotty
+
+hello :: ActionM ()
+hello = do
+  text "hello world!"
+
+routes :: ScottyM ()
+routes = do
+  get "/hello" hello
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Starting Server..."
+  scotty 3000 routes
